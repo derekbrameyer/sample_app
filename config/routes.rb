b46c847f4b82d8ea9266083b17956f7efa243a4e
Derek <dkb4j@virginia.edu>
2010-04-06 12:58:38 -0400

    Finished first cut of the User model

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ ActionController::Routing::Routes.draw do |map|
 	map.about	'/about',	:controller => 'pages', :action => 'about'
 	map.help	'/help',	:controller => 'pages', :action => 'help'
 	map.signup	'/signup',	:controller => 'users', :action => 'new'
+	map.resources	:users
 	
 	# You can have the root of your site routed with map.root
 	# Just remember to delete public/index.html
