f21bd99f71437228773d0d1dfc15023e8862a058
Derek <dkb4j@virginia.edu>
2010-04-05 20:02:23 -0400

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ ActionController::Routing::Routes.draw do |map|
 	map.contact	'/contact',	:controller => 'pages', :action => 'contact'
 	map.about	'/about',	:controller => 'pages', :action => 'about'
 	map.help	'/help',	:controller => 'pages', :action => 'help'
+	map.signup	'/signup',	:controller => 'users', :action => 'new'
 	
 	# You can have the root of your site routed with map.root
 	# Just remember to delete public/index.html
